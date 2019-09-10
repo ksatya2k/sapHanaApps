bd343621b593b04f8206ad1c8571f4527dd60b47
ksatya2k <ksatya2k@gmail.com>
2019-09-10 03:12:21 +0000

    Commit after Node Module

---
+++ srv/router/routes/myNode.js
@@ -0,0 +1,15 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-undef:0, no-process-exit:0*/
+/*eslint-env node, es6 */
+"use strict";
+
+var express = require("express");
+var async = require("async");
+
+module.exports = function() { 
+	var app = express.Router();
+	//Hellow Router
+	app.get("/Hi", (req, res) => {
+	res.send("Hello World Node.js");	
+	});
+	return app;
+};
